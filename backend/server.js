0e9dd97594718b9819cb2b035490ae465003ecaa
Krishna Bata <tusharbata12345@gmail.com>
2025-03-12 20:36:03 +0530

    fixing server.js

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,15 @@ app.use(express.json());
 app.use(cors());
 
 //api endpoints
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
+
 
 app.listen(port, () => {
   console.log("Server started on PORT : " + port);
